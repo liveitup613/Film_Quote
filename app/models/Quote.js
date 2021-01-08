b85432543203ffbd9efcf0b400891f3b9cf06d62
liveitup613 <liveitup613@outlook.com>
2021-01-08 11:41:53 -0800

    update

--- app/models/Quote.js
+++ app/models/Quote.js
@@ -14,20 +14,42 @@ class Quote {
 
         var searchQuery = '';
 
-        console.log(resultArray.length);
-
         for (var index in resultArray) {
             if (index != 0)
                 searchQuery += ' or ';            
             searchQuery += "LOWER(Title) like '%" + resultArray[index] + "%' or LOWER(Reference) like '%"  + resultArray[index] + "%' or LOWER(Explanation) like '%" + resultArray[index] + "%'";
             
         }
-        
+
         return new Promise((resolve, reject) => {
             conn.query(`select * from quotes where ${searchQuery}`, (error, rows) => {
                 db().close();
                 if (error) reject(error);
-                else resolve(rows);
+                else {
+                    rows.map((element, index) => {
+                        var title_cnt = 0;
+                        var exp_cnt = 0;
+                        var ref_cnt = 0;
+                        for(var index in resultArray) {
+                            var filter = resultArray[index];
+
+                            if (element['title'].toLowerCase().includes(filter))
+                                title_cnt++;
+                            if (element['reference'].toLowerCase().includes(filter))
+                                ref_cnt++;
+                            if (element['explanation'].toLowerCase().includes(filter))
+                                exp_cnt++;
+                        }
+
+                        var max_cnt = Math.max(exp_cnt, title_cnt, ref_cnt);
+                        element['contain_cnt'] = max_cnt;
+                    });
+
+                    rows.sort(function(a, b) {
+                        return b['contain_cnt'] - a['contain_cnt'];
+                    });
+                    resolve(rows);
+                }
             });
         });
     }
